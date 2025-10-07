100890c83d9e66db6476b54813e708958b8524c3
Joaquin9999 <joaquin.castano@upb.edu.co>
2025-10-07 09:56:25 -0500

    feat:Creacion del textbox de la vista de tienda

--- app/src/main/java/com/merqueloapp/ui/screens/StoresScreen.kt
+++ app/src/main/java/com/merqueloapp/ui/screens/StoresScreen.kt
@@ -3,8 +3,16 @@ package com.merqueloapp.ui.screens
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.ui.unit.dp
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.merqueloapp.ui.components.AppBottomBar
@@ -21,9 +29,25 @@ fun StoresScreen(
     ) { inner ->
         Box(
             modifier = Modifier.fillMaxSize().padding(inner),
-            contentAlignment = Alignment.Center
+            contentAlignment = Alignment.TopCenter
         ) {
-            // Contenido futuro de Tiendas
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 32.dp)
+            ) {
+                Text("Agrega tus tiendas favoritas")
+                Spacer(modifier = Modifier.height(16.dp))
+                val (text, setText) = remember { mutableStateOf("") }
+                OutlinedTextField(
+                    value = text,
+                    onValueChange = setText,
+                    modifier = Modifier,
+                    placeholder = { Text("Nombre de la tienda") }
+                )
+                // Contenido futuro de Tiendas
+            }
         }
     }
 }
